d4bba4d13455108ac5f7e817243225d79be416c0
yuyanjun <yuyanjun@zego.im>
2024-11-27 12:10:21 +0800

    Fix test zim types crash on debug mode

--- lib/src/internal/test_sdk.dart
+++ lib/src/internal/test_sdk.dart
@@ -12,16 +12,60 @@ void testZIMTypes() {
     subTag: 'test',
   );
 
-  assert(ZegoSignalingPluginConnectionState.values.length ==
-      ZIMConnectionState.values.length);
-  assert(ZegoSignalingPluginConnectionAction.values.length ==
-      ZIMConnectionEvent.values.length);
-  assert(ZegoSignalingPluginRoomAction.values.length ==
-      ZIMRoomEvent.values.length);
-  assert(
-      ZegoSignalingPluginRoomState.values.length == ZIMRoomState.values.length);
-  assert(ZegoSignalingPluginCallUserState.values.length ==
-      ZIMCallUserState.values.length);
+  if (ZegoSignalingPluginConnectionState.values.length !=
+      ZIMConnectionState.values.length) {
+    ZegoSignalingLoggerService.logError(
+      'ZegoSignalingPluginConnectionState != ZIMConnectionState, '
+      'ZegoSignalingPluginConnectionState:${ZegoSignalingPluginConnectionState.values}, '
+      'ZIMConnectionState:${ZIMConnectionState.values}, ',
+      tag: 'signaling',
+      subTag: 'test',
+    );
+  }
+
+  if (ZegoSignalingPluginConnectionAction.values.length !=
+      ZIMConnectionEvent.values.length) {
+    ZegoSignalingLoggerService.logError(
+      'ZegoSignalingPluginConnectionAction != ZIMConnectionEvent, '
+      'ZegoSignalingPluginConnectionAction:${ZegoSignalingPluginConnectionAction.values}, '
+      'ZIMConnectionEvent:${ZIMConnectionEvent.values}, ',
+      tag: 'signaling',
+      subTag: 'test',
+    );
+  }
+
+  if (ZegoSignalingPluginRoomAction.values.length !=
+      ZIMRoomEvent.values.length) {
+    ZegoSignalingLoggerService.logError(
+      'ZegoSignalingPluginRoomAction != ZIMRoomEvent, '
+      'ZegoSignalingPluginRoomAction:${ZegoSignalingPluginRoomAction.values}, '
+      'ZIMRoomEvent:${ZIMRoomEvent.values}, ',
+      tag: 'signaling',
+      subTag: 'test',
+    );
+  }
+
+  if (ZegoSignalingPluginRoomState.values.length !=
+      ZIMRoomState.values.length) {
+    ZegoSignalingLoggerService.logError(
+      'ZegoSignalingPluginRoomState != ZIMRoomState, '
+      'ZegoSignalingPluginRoomState:${ZegoSignalingPluginRoomState.values}, '
+      'ZIMRoomState:${ZIMRoomState.values}, ',
+      tag: 'signaling',
+      subTag: 'test',
+    );
+  }
+
+  if (ZegoSignalingPluginCallUserState.values.length !=
+      ZIMCallUserState.values.length) {
+    ZegoSignalingLoggerService.logError(
+      'ZegoSignalingPluginCallUserState != ZIMCallUserState, '
+      'ZegoSignalingPluginCallUserState:${ZegoSignalingPluginCallUserState.values}, '
+      'ZIMCallUserState:${ZIMCallUserState.values}, ',
+      tag: 'signaling',
+      subTag: 'test',
+    );
+  }
 
   ZegoSignalingLoggerService.logInfo(
     'test zim types done',
