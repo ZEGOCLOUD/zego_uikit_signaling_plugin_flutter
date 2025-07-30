02e28a4b43d2c5aa3698ccbc4ab4dba9e264c795
yuyanjun <yuyanjun@zego.im>
2025-07-30 11:33:02 +0800

    update log dependency

--- lib/src/log/logger_service.dart
+++ lib/src/log/logger_service.dart
@@ -9,7 +9,7 @@ class ZegoSignalingLoggerService {
   static bool isZegoLoggerInit = false;
 
   /// init log
-  Future<void> initLog({String folderName = 'uikit'}) async {
+  Future<void> initLog() async {
     if (isZegoLoggerInit) {
       return;
     }
@@ -30,8 +30,9 @@ class ZegoSignalingLoggerService {
               directoryStructure: DirectoryStructure.SINGLE_FILE_FOR_DAY,
               logTypesEnabled: ['device', 'network', 'errors'],
               logFileExtension: LogFileExtension.LOG,
-              logsWriteDirectoryName: 'zego_prebuilt/$folderName',
-              logsExportDirectoryName: 'zego_prebuilt/$folderName/Exported',
+              logsWriteDirectoryName: 'zego_prebuilt',
+              logsExportDirectoryName: 'zego_prebuilt/Exported',
+              useCachesDirectory: true,
               debugFileOperations: true,
               isDebuggable: true)
           .then((value) {
